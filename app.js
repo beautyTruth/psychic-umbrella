4ee01c3dcb08a288f1881d89483dfadb5a9cc71b
Phil <happy.melodist@gmail.com>
2021-11-18 19:52:10 -0600

    complete markup and styling review, noting the neumorphism applied by adding classes to focused keys

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const keyContainers = document.querySelectorAll(".keys");
-let textArea = document.querySelector(".display textarea");
-// console.log(keyContainers);
-// console.log(keyContainers[0]);
-// console.log(keyContainers[0].children);
+// const keyContainers = document.querySelectorAll(".keys");
+// let textArea = document.querySelector(".display textarea");
+// // console.log(keyContainers);
+// // console.log(keyContainers[0]);
+// // console.log(keyContainers[0].children);
 
-keyContainers.forEach(function (key) {
-  key.addEventListener("click", function (e) {
-    // console.log(e);
-    // console.log(e.path);
-    // console.log(e.path[0]);
-    // console.log(e.path[0].value);
-    // console.log(e.path[0].value.toLowerCase());
+// keyContainers.forEach(function (key) {
+//   key.addEventListener("click", function (e) {
+//     // console.log(e);
+//     // console.log(e.path);
+//     // console.log(e.path[0]);
+//     // console.log(e.path[0].value);
+//     // console.log(e.path[0].value.toLowerCase());
 
-    if (!e.path[0].value) return;
+//     if (!e.path[0].value) return;
 
-    let keyClicked = e.path[0].value.toLowerCase();
-    textArea.value += keyClicked;
-  });
-});
+//     let keyClicked = e.path[0].value.toLowerCase();
+//     textArea.value += keyClicked;
+//   });
+// });
+
+// I'm not angry that you lied; I'm angry that I can never trust you again
+
+/*
+my code
+*/
